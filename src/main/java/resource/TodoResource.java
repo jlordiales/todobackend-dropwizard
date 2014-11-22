b293dfe8a466070351b4aa3ca52f3563ac7b2d61
Jose Luis Ordiales Coscia <jlordiales@gmail.com>
2014-11-22 17:46:32 -0200

    Added CORS and Post method

--- src/main/java/resource/TodoResource.java
+++ src/main/java/resource/TodoResource.java
@@ -1,9 +1,11 @@
 package resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
@@ -13,4 +15,11 @@ public class TodoResource {
     public String hello() {
         return "Hello World";
     }
+
+    @POST
+    public Response post() {
+        return Response.ok().build();
+    }
+
+    
 }
--- src/main/java/service/TodoApplication.java
+++ src/main/java/service/TodoApplication.java
@@ -1,10 +1,16 @@
 package service;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration.Dynamic;
+
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.java8.Java8Bundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import resource.TodoResource;
 
 public class TodoApplication extends Application<Configuration> {
@@ -15,7 +21,18 @@ public class TodoApplication extends Application<Configuration> {
 
     @Override
     public void run(Configuration configuration, Environment environment) throws Exception {
+        configureCors(environment);
         environment.jersey().register(new TodoResource());
+    }
+
+    private void configureCors(Environment environment) {
+        Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        filter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        filter.setInitParameter("allowCredentials", "true");
     }
 
     public static void main(String[] args) throws Exception {
